ac32f899cf653964e50a82c75472ef7c4ed0eab9
Jack Branch <39771802+pliantmeerkat@users.noreply.github.com>
2018-08-29 15:09:11 +0100

    more studff

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,7 +14,6 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, :type=> :controller
 
   config.use_transactional_fixtures = true

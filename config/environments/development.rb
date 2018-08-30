e3f20e48754b8f14435ceb5e0a3cdac4d80ad257
Daniel den Hartog <daniel.mista@gmail.com>
2018-08-30 17:04:38 +0100

    added heroku as host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
 
-  config.action_mailer.default_url_options = { :host => "polar-ravine-85745.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "localhost", port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end

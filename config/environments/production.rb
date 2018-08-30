88b6fca201d1f464bae842e08b3f525932c6da35
Daniel den Hartog <daniel.mista@gmail.com>
2018-08-30 17:07:05 +0100

    added heroku as host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   config.action_mailer.default_url_options = { :host => "polar-ravine-85745.herokuapp.com" }
-  config.action_mailer.delivery_method = :smtp
+
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'

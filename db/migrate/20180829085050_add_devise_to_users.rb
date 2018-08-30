9e4047d8a4b6b4c1a325e1b006f22c05a9963a97
Daniel den Hartog <daniel.mista@gmail.com>
2018-08-30 10:06:34 +0100

    changes

--- db/migrate/20180829085050_add_devise_to_users.rb
+++ db/migrate/20180829085050_add_devise_to_users.rb
@@ -32,6 +32,10 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.1]
       t.string   :unlock_token # Only if unlock strategy is :email or :both
       t.datetime :locked_at
 
+      t.string :username
+      t.string :image_url
+      t.string :bio_desc
+
       # Uncomment below if timestamps were not included in your original model.
       # t.timestamps null: false
     end

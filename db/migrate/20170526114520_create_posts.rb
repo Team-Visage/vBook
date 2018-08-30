f86f0e3c3bd6201b0315b5b55448eb42260beefc
Jack Branch <39771802+pliantmeerkat@users.noreply.github.com>
2018-08-30 14:53:17 +0100

    fixing migrations

--- db/migrate/20170526114520_create_posts.rb
+++ db/migrate/20170526114520_create_posts.rb
@@ -2,8 +2,8 @@ class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
       t.string :message
-
       t.timestamps
     end
+    create_table :users
   end
 end

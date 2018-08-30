e9470e652e8947e6135f5229d721a12f5b1116b3
Jack Branch <39771802+pliantmeerkat@users.noreply.github.com>
2018-08-30 14:57:35 +0100

    fixing migrations

--- db/migrate/20180830105153_add_username_to_users.rb
+++ db/migrate/20180830105153_add_username_to_users.rb
@@ -1,8 +1,8 @@
 class AddUsernameToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :username, :string
-    add_index :users, :username
     add_column :users, :bio_desc, :string
     add_column :users, :img_url, :string
+    add_index :users, :username, unique: true
   end
 end
--- db/migrate/20180830111808_add_username_to_posts.rb
+++ db/migrate/20180830111808_add_username_to_posts.rb
@@ -1,5 +1,8 @@
 class AddUsernameToPosts < ActiveRecord::Migration[5.1]
   def change
-    add_column :posts, :user_id, :string
+    add_column :posts, :user_id,    :integer
+    add_column :posts, :post_likes, :integer, default: 0
+    add_column :posts, :post_flags, :integer
+    add_column :posts, :post_tags, :string
   end
 end

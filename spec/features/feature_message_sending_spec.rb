5f793a1a9607fbde70afd8678c5c2dad11b273fc
Jack Branch <39771802+pliantmeerkat@users.noreply.github.com>
2018-09-05 11:20:55 +0100

    testing travis

--- spec/features/feature_message_sending_spec.rb
+++ spec/features/feature_message_sending_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 RSpec.feature 'Public Messaging', type: :feature do
   before(:each) do
     add_test_data_to_db
+    visit('/')
+    login_to_acebook
     visit('/posts')
   end
   scenario 'User can see list of messsages' do
@@ -9,12 +11,11 @@ RSpec.feature 'Public Messaging', type: :feature do
     expect(page).to have_content('hello i am a test message')
   end
   scenario 'User cannot post message if not logged in' do
-    click_link 'New post'
+    logout_of_acebook
+    visit('/posts')
     expect(page).to have_content('You need to sign in or sign up before continuing')
   end
   scenario 'Logged in user can post a message and see it displayed' do
-    visit('/')
-    login_to_acebook
     visit('/posts')
     send_test_msg
     expect(page).to have_content('Hello, world!')

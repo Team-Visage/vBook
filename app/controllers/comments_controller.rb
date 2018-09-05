cf96919d4dbb39279e542a41508a34439e9f25cb
edthomas93 <et1270@my.bristol.ac.uk>
2018-09-05 11:17:09 +0100

    implimented comments / no tests

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+
+  def index
+
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    redirect_to posts_path
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body).merge(username: current_user.username)
+  end
+end

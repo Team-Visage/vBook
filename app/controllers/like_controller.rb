e92000dacd8da90cdbff5975ae02673e1f25e942
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-09-06 14:55:24 +0100

    users can only like a post once

--- app/controllers/like_controller.rb
+++ app/controllers/like_controller.rb
@@ -1,8 +1,19 @@
+
 class LikeController < ApplicationController
   def create
     @post = Post.find(params['posts'['post']]['id'].to_i)
-    @post.post_likes += 1
-    @post.save
-    redirect_to posts_path
+    # @post.post_likes += 1
+    # @post.liked_by.push(current_user.id)
+    # p 'this is the array of likes'
+    # p @post.liked_by
+    if @post.liked_by.include?(current_user.id.to_s)
+      redirect_to posts_path
+    else
+      @post.post_likes += 1
+      @post.liked_by.push(current_user.id)
+      @post.save
+      redirect_to posts_path
+    end
+    p @post.liked_by
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180905085619) do
+ActiveRecord::Schema.define(version: 20180906131821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20180905085619) do
     t.integer "post_likes", default: 0
     t.integer "post_flags"
     t.string "post_tags"
+    t.text "liked_by", default: [], array: true
   end
 
   create_table "users", force: :cascade do |t|

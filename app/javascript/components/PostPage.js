f836ee6f97c9208efa918bc525e62fae1db63dd8
Jack Branch <39771802+pliantmeerkat@users.noreply.github.com>
2018-09-06 18:11:46 +0100

    adding react post stuff

--- app/javascript/components/PostPage.js
+++ app/javascript/components/PostPage.js
@@ -1,5 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Post from './Post'
 class PostPage extends React.Component {
   constructor(){
     super()
@@ -19,7 +20,7 @@ class PostPage extends React.Component {
         <button id='newPostActiveButton' onClick={() => this.setState({newPostActive: true})}>New Post</button>
       </div>
         {this.newPost()}
-        {this.renderSinglePost()}
+        {this.renderAllPosts()}
       </React.Fragment>
     );
   }
@@ -31,67 +32,13 @@ class PostPage extends React.Component {
           return (
             <Post
               key={post.id}
-              postCreator={post.user_id}
-              postDate={post.created_at}
-              postBody={post.message}
-              postComments={post.comments}
+              postAuthor={post.user_id} 
+              post = {post}
             />
           )
         })}
       </div>
     )
-  }
-
-  renderSinglePost() {
-    return (
-      <div className = 'singlePostDiv'>
-      {this.postHead()}
-      {this.postBody()}
-      {this.commentsDisplay()}
-    </div>
-    )
-  }
-
-  postHead(){
-    return(
-      <div className='postHead'>
-        <h3>{this.props.postUser.username}</h3>
-        <img src={this.props.postUser.image_url} height='50px'/>
-        Posted at {this.props.post.created_at}
-      </div>
-    )
-  }
-
-  postBody(){
-    return(
-      <div className='postBody'>
-        <p> "{this.props.post.message}" </p>
-        <p> <strong> Liked {this.props.post.post_likes} times </strong> </p>
-        {this.likePost()}
-      </div>
-    )
-  }
-
-  displayComment(comment){
-    return(
-      <div className='comment'>
-        <p>{comment.username}: {comment.body}</p>
-      </div>
-    )
-  }
-
-  commentsDisplay(){
-    var commentList = this.props.postComments
-    if(commentList != null){
-      return(
-        <div className='commentsDisplay'>
-          {commentList.map(function(comment){
-            return <p>{comment.username}: "{comment.body}"</p>
-          })}
-          {this.commentSubmit()}
-        </div>
-      )
-    }
   }
 
   newPost() {
@@ -107,44 +54,6 @@ class PostPage extends React.Component {
       )
     } else {
       console.log(this.state.newPostActive)
-    }
-  }
-
-  likePost(){
-    return(
-      <div>
-        <form method='post' action={`/like?post[id]=${this.props.post.id}`}>
-          <button id={`like${this.props.post.id}`} type='Submit'>Like</button>
-        </form>
-      </div>
-    )
-  }
-
-  onSubmit(event) {
-    event.preventDefault()
-  }
-
-  postLike(){
-
-  }
-
-  commentSubmit(){
-    var id = this.props.post.id
-    if(this.state.newComment){
-      return(
-        <div >
-          <form method='post'>
-          <textarea id={`textarea${id}`} value={this.state.commentText} onChange={this.updateComment.bind(this)}> placeholder></textarea>
-          <p><button id={`submit${id}`} formAction = {`posts/${id}/comments?comment[body]=${this.state.commentText}`}>Sumbit</button></p>
-          </form>
-        </div>
-      )
-    } else{
-      return (
-          <div>
-              <button id={`comment${this.props.post.id}`} type='Submit' onClick={() => this.setState({newComment: true})}>New Comment</button>
-          </div>
-      )
     }
   }
 

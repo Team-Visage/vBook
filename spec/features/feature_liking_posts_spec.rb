5de5fc6774d73016b4c75bbf42752b70a4278eec
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-09-06 15:16:00 +0100

    added post liking tests

--- spec/features/feature_liking_posts_spec.rb
+++ spec/features/feature_liking_posts_spec.rb
@@ -6,4 +6,12 @@ RSpec.feature 'Post likes', type: :feature do
     like_a_post
     expect(page).to have_content('Liked 1 times')
   end
+  scenario 'user can only like a post once' do
+    signup_login_and_view_posts
+    like_a_post
+    like_a_post 
+    expect(page).to have_content('Liked 1 times')
+    expect(page).to_not have_content('Liked 2 times')
+    save_and_open_page
+  end
 end

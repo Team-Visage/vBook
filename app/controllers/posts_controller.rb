37adb09c1a84df96cd92921ca32df5154923cb59
Daniel den Hartog <daniel.mista@gmail.com>
2018-09-06 08:57:57 +0100

    edit and deleteing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,22 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(:id)
+    @post.update(post_params)
+    redirect_to posts_url
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_url
+  end
+
   private
 
   def post_params
